9e6ab290ea8f295e468b5da20c9f571bd19ea172
Andriy <asus261235@gmail.com>
2023-06-26 22:07:43 +0200

    Start. Learn switch

---
+++ js/01-switch.js
@@ -0,0 +1,80 @@
+// const stars = 1;
+// let price;
+
+// if (stars === 1) {
+//   price = 20;
+// } else if (stars === 2) {
+//   price = 30;
+// } else if (stars === 3) {
+//   price = 50;
+// } else if (stars === 4) {
+//   price = 70;
+// } else if (stars === 5) {
+//   price = 120;
+// } else {
+//   console.log("Hotel with that amount of stars does not exist!!!");
+// }
+
+// switch (stars) {
+//   case 1:
+//     price = 20;
+//     //console.log("dkfjjadsf");
+//     break;
+
+//   case 2:
+//     price = 30;
+//     break;
+
+//   case 3:
+//     price = 50;
+//     break;
+
+//   case 4:
+//     price = 70;
+//     break;
+
+//   case 5:
+//     price = 120;
+//     break;
+
+//   default:
+//     console.log("Hotel with that amount of stars does not exist!!!");
+// }
+
+// console.log(price);
+
+// const stars = 7;
+// let price;
+
+// if (stars === 1 || stars === 2) {
+//   price = 20;
+// } else if (stars === 3 || stars === 4) {
+//   price = 30;
+// } else if (stars === 5) {
+//   price = 120;
+// } else {
+//   console.log("Hotel with that amount of stars does not exist!!!");
+// }
+
+// console.log(price);
+
+// switch (stars) {
+//   case 1:
+//   case 2:
+//     price = 20;
+//     break;
+
+//   case 3:
+//   case 4:
+//     price = 30;
+//     break;
+
+//   case 5:
+//     price = 120;
+//     break;
+
+//   default:
+//     console.log("Hotel with that amount of stars does not exist!!!");
+// }
+
+// console.log(price);
